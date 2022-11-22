acea5892714ff3cf0a4bc8a30ae3f5bae27f33f0
syntaxsemantixs <gormeno2030@gmail.com>
2022-11-22 00:09:35 -0500

    finished code and added comments

--- script.js
+++ script.js
@@ -16,11 +16,15 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
+// user input variables
+
   var userInput;
   var numbers;
   var specialChar;
   var upperCase;
   var lowerCase;
+
+//individual and single scenarios
 
 lowerChar = "abcdefghijklmnopqrstuvwxyz"
 
@@ -29,6 +33,8 @@ upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numChar = "1234567890"
 
 specChar = "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+//all character scenarios
 
 allcharacters = lowerChar + upperChar + numChar + specChar
 
@@ -46,22 +52,19 @@ upperNumSpec = upperChar + numChar + specChar
 
 upperNum = upperChar + numChar
 
-UpperSpec = upperChar + specChar
+upperSpec = upperChar + specChar
 
 numSpec = numChar + specialChar
 
 
-  var values;
-
 function generatePassword() {
-  console.log("Hello")
-
-
+  // userinput prompt
   userInput = window.prompt("Please choose your password length. Must be between (8 - 128 characters).")
 
   if (!userInput){
     return "Generator cancelled.\nPress the button to try again.";
-  } else if (userInput > 8 && userInput < 128) {
+    //all prompts based off userinput
+  } else if (userInput >= 8 && userInput <= 128) {
     lowerCase = window.confirm("Would you like to use LOWERCASE characters?")
     upperCase = window.confirm("Would you like to use UPPERCASE characters?")
     numbers = window.confirm("Would you like to use NUMBERS characters?")
@@ -70,10 +73,12 @@ function generatePassword() {
     generatePassword()
   };
 
-  // no user data
+  // no user data results
   if (!lowerCase && !upperCase && !numbers && !specialChar) {
     return "No characters were selected.\nPress the button to try again.";
-  }else if (lowerCase && upperCase && numbers && specialChar){
+  }
+  //all characters results
+  else if (lowerCase && upperCase && numbers && specialChar){
     var result = ""
     for (let i=0; i < userInput; i++) {
       result += allcharacters[Math.floor(Math.random() * allcharacters.length)]
@@ -81,4 +86,103 @@ function generatePassword() {
     }
     return result
   }
+  //lower results
+  else if (lowerCase && upperCase && numbers && !specialChar){
+    var result = ""
+    for (let i=0; i < userInput; i++) {
+      result += lowerUpperNum[Math.floor(Math.random() * lowerUpperNum.length)]
+      console.log(result)
+    }
+    return result
+}else if (lowerCase && !upperCase && numbers && specialChar){
+  var result = ""
+  for (let i=0; i < userInput; i++) {
+    result += lowerNumSpec[Math.floor(Math.random() * lowerNumSpec.length)]
+    console.log(result)
+  }
+  return result
+}else if (lowerCase && upperCase && !numbers && !specialChar){
+  var result = ""
+  for (let i=0; i < userInput; i++) {
+    result += lowerUpper[Math.floor(Math.random() * lowerUpper.length)]
+    console.log(result)
+  }
+  return result
+}else if (lowerCase && !upperCase && numbers && !specialChar){
+  var result = ""
+  for (let i=0; i < userInput; i++) {
+    result += lowerNum[Math.floor(Math.random() * lowerNum.length)]
+    console.log(result)
+  }
+  return result
+}else if (lowerCase && !upperCase && !numbers && specialChar){
+  var result = ""
+  for (let i=0; i < userInput; i++) {
+    result += lowerSpec[Math.floor(Math.random() * lowerSpec.length)]
+    console.log(result)
+  }
+  return result
+}
+//upper results
+else if (!lowerCase && upperCase && numbers && specialChar){
+  var result = ""
+  for (let i=0; i < userInput; i++) {
+    result += upperNumSpec[Math.floor(Math.random() * upperNumSpec.length)]
+    console.log(result)
+  }
+  return result
+}else if (!lowerCase && upperCase && numbers && !specialChar){
+  var result = ""
+  for (let i=0; i < userInput; i++) {
+    result += upperNum[Math.floor(Math.random() * upperNum.length)]
+    console.log(result)
+  }
+  return result
+}else if (!lowerCase && upperCase && !numbers && specialChar){
+  var result = ""
+  for (let i=0; i < userInput; i++) {
+    result += upperSpec[Math.floor(Math.random() * upperSpec.length)]
+    console.log(result)
+  }
+  return result
+}
+// number and special result
+else if (!lowerCase && !upperCase && numbers && specialChar){
+  var result = ""
+  for (let i=0; i < userInput; i++) {
+    result += numSpec[Math.floor(Math.random() * numSpec.length)]
+    console.log(result)
+  }
+  return result
+}
+//single resutlts
+else if (lowerCase && !upperCase && !numbers && !specialChar){
+  var result = ""
+  for (let i=0; i < userInput; i++) {
+    result += lowerChar[Math.floor(Math.random() * lowerChar.length)]
+    console.log(result)
+  }
+  return result
+}else if (!lowerCase && upperCase && !numbers && !specialChar){
+  var result = ""
+  for (let i=0; i < userInput; i++) {
+    result += upperChar[Math.floor(Math.random() * upperChar.length)]
+    console.log(result)
+  }
+  return result
+}else if (!lowerCase && !upperCase && numbers && !specialChar){
+  var result = ""
+  for (let i=0; i < userInput; i++) {
+    result += numChar[Math.floor(Math.random() * numChar.length)]
+    console.log(result)
+  }
+  return result
+}else if (!lowerCase && !upperCase && !numbers && specialChar){
+  var result = ""
+  for (let i=0; i < userInput; i++) {
+    result += specChar[Math.floor(Math.random() * numChar.length)]
+    console.log(result)
+  }
+  return result
+}
 }
